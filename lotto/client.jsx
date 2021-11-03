b919ae486b2d7f2e1beb67edb21f25fd64f1fa52
rkrkdldkd@naver.com <rkrkdldkd@naver.com>
2021-11-04 01:46:53 +0900

    lotto game first upload

---
+++ lotto/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const NumberBaseball = require('./lotto');
+
+ReactDOM.render(<NumberBaseball/>,document.querySelector('#root'));
